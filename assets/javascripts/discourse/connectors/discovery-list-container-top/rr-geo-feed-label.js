b585a904a2373308313e7203f6b6cf117a633c36
Gary Stimpson <garystimpson03@gmail.com>
2026-04-29 13:07:35 -0400

    Human removes assets because I do not know how to...

--- assets/javascripts/discourse/connectors/discovery-list-container-top/rr-geo-feed-label.js
+++
@@ -1,9 +0,0 @@
-// Superseded by rr-geo-feed-label.gjs in v0.3.0. Kept as a stub because the
-// workspace doesn't allow file deletion. Discourse may load both -- the .gjs
-// component takes precedence.
-import Component from "@glimmer/component";
-import { service } from "@ember/service";
-
-export default class RrGeoFeedLabelLegacy extends Component {
-  @service currentUser;
-}
